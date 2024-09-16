b4614a96d7445f806ea12b696b2854da1deb458e
Edwin <Edwinallen94@aol.com>
2024-09-16 00:19:05 -0400

    checking if all numbers are divisable by 5

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+// Part 1: Math Problems
+// naning variables
+const numOne = 9; // This is the first number
+const numTwo = 14; // This is the second number
+const numThree = 23; // This is the third number
+const numFour = 8; // This is the fourth number
 
+//Checking if all numbers are divisible by 5
+const allNumbersDivisibleBy5 =
+  numOne % 5 === 0 &&
+  numTwo % 5 === 0 &&
+  numThree % 5 === 0 &&
+  numFour % 5 === 0;
+console.log("All numbers are divisible by 5:", allNumbersDivisibleBy5); //true if all numbers are divisible
