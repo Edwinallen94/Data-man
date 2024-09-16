b78f77d6ebda8bb5deb875216c1b7ff94373b329
Edwin <Edwinallen94@aol.com>
2024-09-16 01:30:44 -0400

    checked if first number is larger than the last / boolean

--- app.js
+++ app.js
@@ -12,3 +12,20 @@ const allNumbersDivisibleBy5 =
   numThree % 5 === 0 &&
   numFour % 5 === 0;
 console.log("All numbers are divisible by 5:", allNumbersDivisibleBy5); //true if all numbers are divisible
+
+// Checking if the first number is larger than the last number
+const firstNumberLargerThanLast = numOne > numFour;
+console.log(
+  "The first number is larger than the last number:",
+  firstNumberLargerThanLast
+); //(boolean) true or false
+
+//find the remainder when divided by numFour; // Then multiply the result by numThree
+const numberResult = ((numTwo - numOne) * numThree) % numFour; // Subtract numOne from numTwo
+console.log("Result of number arithmetic chain:", numberResult); // Print the result
+
+// I Renamed the variable and check if any number is greater than 25
+// Instead of using !, I check each number directly
+const anyNumberGreaterThan25 =
+  numOne > 25 || numTwo > 25 || numThree > 25 || numFour > 25;
+console.log("Is any number greater than 25:", anyNumberGreaterThan25); // Should print true if any number is greater than 25
